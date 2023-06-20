12e20b5640e6474ad8b28992004bddeb172b07e5
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-20 12:21:38 +0530

    intro

---
+++ intro.php
@@ -0,0 +1,49 @@
+<?php 
+//                                         php programming
+//                                          ------------ 
+
+// What Can PHP Do?
+
+    // * PHP can generate dynamic page content
+    // * PHP can create, open, read, write, delete, and close files on the server
+    // * PHP can collect form data
+    // * PHP can send and receive cookies
+    // * PHP can add, delete, modify data in your database
+    // * PHP can be used to control user-access
+    // * PHP can encrypt data
+
+// Why PHP?
+
+    // * PHP runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+    // * PHP is compatible with almost all servers used today (Apache, IIS, etc.)
+    // * PHP supports a wide range of databases
+    // * PHP is free
+    // * PHP is easy to learn and runs efficiently on the server side
+
+
+// Basic php Syntax
+
+    /*
+    <?php
+        // php codes
+    ?>
+     */
+
+// comments
+
+    // single line comment
+
+        // This is a single-line comment
+
+        # This is also a single-line comment
+    
+    // multiline comments
+    
+        /*
+        This is a multiple-lines comment block
+        that spans over multiple
+        lines
+        */
+
+
+?>
