fc28d7305380866c011e6c94a7ca3eecc1b41576
Sanchitha Udana Wijesundara <109952575+SanchithaUdana@users.noreply.github.com>
2023-06-21 06:18:52 +0530

    math function

---
+++ math_function.php
@@ -0,0 +1,56 @@
+<?php 
+
+//                                          PHP Math
+//                                          --------
+
+// pi function
+
+echo pi();
+
+echo "<br>";
+
+// min() and max() Functions
+
+echo (min(12,23,12,45,67,44,));
+echo "<br>";
+echo (max(12,45,32,45,45,32,45));
+echo "<br>";
+
+// PHP abs() Function
+    // that function return positive number of the negative number
+
+echo abs(-12); // output is 12
+
+echo "<br>";
+
+// PHP sqrt() Function
+
+echo sqrt(64); // output is 8
+
+echo "<br>";
+
+
+// PHP round() Function
+echo round(0.06); // output is 1
+
+// Random Numbers
+
+echo rand(); // output is a random number
+echo "<br>";
+echo rand(1,100); // return a random number between 1 and 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
